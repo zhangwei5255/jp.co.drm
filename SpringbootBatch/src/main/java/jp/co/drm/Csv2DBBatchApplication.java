0256c44759e42ae5a4e3ca7b52e01a1aed00efca
zhang <zhang@LTJP-SERVER08>
2017-07-13 15:20:20 +0900

    CSV2DB

---
+++ SpringbootBatch/src/main/java/jp/co/drm/Csv2DBBatchApplication.java
@@ -0,0 +1,114 @@
+package jp.co.drm;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.batch.MyBatisBatchItemWriter;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+
+import jp.co.drm.base.integration.mybatis.entity.Person;
+import jp.co.drm.batch.config.processor.PersonItemProcessor;
+
+//@SpringBootApplication(scanBasePackages={"com.sample.commons", "com.sample.product"})
+@SpringBootApplication(scanBasePackages = { "jp.co.drm" }) // デフォルト：当クラスのpackage
+@MapperScan("jp.co.drm.**.integration.mybatis.dao")
+@EnableBatchProcessing
+public class Csv2DBBatchApplication {
+
+	 private final static String FILE_NAME = "sample-data.csv";
+
+	@Autowired
+	private JobBuilderFactory jobs;
+
+	@Autowired
+	private StepBuilderFactory steps;
+
+	@Autowired
+	private SqlSessionFactory sqlSessionFactory;
+
+	@Bean
+	public Job jobCsv2Db() throws Exception {
+		System.out.println("jobCsv2Db: build");
+		return this.jobs.get("jobCsv2Db").start(stepCsv2Db()).build();
+	}
+
+	@Bean
+	protected Step stepCsv2Db() throws Exception {
+		System.out.println("stepCsv2Db: build");
+		return steps.get("stepCsv2Db").<Person, Person>chunk(10).reader(csvItemReader()).processor(processor())
+				.writer(myBatisItemReader()).build();
+	}
+
+	@Bean
+	// public ItemReader<Person> csvItemReader(String fileName) {
+	public ItemReader<Person> csvItemReader() {
+		FlatFileItemReader<Person> reader = new FlatFileItemReader<>();
+		reader.setResource(new ClassPathResource(FILE_NAME));
+		// reader.setResource(new ClassPathResource(fileName));
+		// reader.setResource(new FileSystemResource(FILE_NAME)); // read system
+		// file.
+		// reader.setLinesToSkip(1); // skip header.
+		reader.setLineMapper(new DefaultLineMapper<Person>() {
+			{
+				setLineTokenizer(new DelimitedLineTokenizer() {
+					{
+						// setDelimiter(DelimitedLineTokenizer.DELIMITER_TAB);
+						// // read tvs.
+						// setNames(new String[]{"firstName", "lastName",
+						// "mail"});
+						setNames(new String[] { "firstName", "lastName" });
+					}
+				});
+				setFieldSetMapper(new BeanWrapperFieldSetMapper<Person>() {
+					{
+						setTargetType(Person.class);
+					}
+				});
+			}
+		});
+
+
+
+		return reader;
+	}
+
+
+    @Bean
+    public PersonItemProcessor processor() {
+        return new PersonItemProcessor();
+    }
+
+
+	@Bean
+	public ItemWriter<Person> myBatisItemReader() {
+		MyBatisBatchItemWriter<Person> writer = new MyBatisBatchItemWriter<Person>();
+		writer.setSqlSessionFactory(sqlSessionFactory);
+		// reader.setParameterValues(parameterValues);
+		writer.setStatementId("jp.co.drm.base.integration.mybatis.dao.PersonDao.insert");
+
+		return writer;
+	}
+
+
+	public static void main(String[] args) throws Exception {
+		// System.exit is common for Batch applications since the exit code can
+		// be used to
+		// drive a workflow
+		System.exit(SpringApplication.exit(SpringApplication.run(Csv2DBBatchApplication.class, args)));
+	}
+
+}
---
+++ SpringbootBatch/src/main/java/jp/co/drm/batch/config/processor/PersonItemProcessor.java
@@ -0,0 +1,26 @@
+package jp.co.drm.batch.config.processor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemProcessor;
+
+import jp.co.drm.base.integration.mybatis.entity.Person;
+
+public class PersonItemProcessor implements ItemProcessor<Person, Person> {
+
+    private static final Logger log = LoggerFactory.getLogger(PersonItemProcessor.class);
+
+    @Override
+    public Person process(final Person person) throws Exception {
+         String firstName = person.getFirstName().toUpperCase();
+         String lastName = person.getLastName().toUpperCase();
+
+         Person transformedPerson = new Person();
+        transformedPerson.setFirstName(firstName);
+        transformedPerson.setLastName(lastName);
+
+        log.info("Converting (" + person + ") into (" + transformedPerson + ")");
+
+        return transformedPerson;
+    }
+}
